0743d77fd11f9f35af6c2dab1f0b999d9f77cb5b
Pits-webdev <pits.webdev@gmail.com>
2024-07-14 20:23:44 +0200

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      /* "2xl": "1536px", */
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    fontFamily: {
+      Inter: "Inter, sans-serif",
+    },
+
+    extend: {
+      colors: {
+        background: "var(--background)",
+        background_light: "var(--backgroundLight)",
+        accent: "var(--accent)",
+        gray: "var(--gray)",
+      },
+    },
+  },
+  plugins: [],
+};
